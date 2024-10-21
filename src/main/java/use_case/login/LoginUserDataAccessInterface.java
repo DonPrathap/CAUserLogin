3b8842eea34be127f1e169f7081500dc32aa65f1
donedirisinghe <don.edrich@gmail.com>
2024-10-21 10:28:47 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Method.
+     * @param name name.
+     */
+    void setCurrentUser(String name);
 }
